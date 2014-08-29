5012400c0781ed0d3419b0f690b8d8dcbd6f6ccd
Ronmi Ren <ronmi.ren@gmail.com>
2014-08-29 10:40:55 +0800

    prepare config object for controller

--- www/index.php
+++ www/index.php
@@ -1,4 +1,6 @@
 <?php
 require(implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), 'vendor', 'autoload.php']));
 define('BASE_DIR', dirname(__DIR__));
-new Fruit\Config(dirname(__DIR__))->getRouter()->route($_SERVER['DOCUMENT_URI']);
+$cfg = new Fruit\Config(dirname(__DIR__));
+Fruit\Seed::fertilize($cfg);
+$cfg->getRouter()->route($_SERVER['DOCUMENT_URI']);
