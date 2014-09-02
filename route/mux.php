c86f751c6d7c67e3c9c4cef4012d6dd5cb7df0e1
owen_lin <owen80127@gmail.com>
2014-09-02 14:52:34 +0800

    fixconfict

--- route/mux.php
+++ route/mux.php
@@ -1,79 +1,124 @@
 <?php
 define('BASE_DIR', dirname(__DIR__)); // set upper directory as base dir
-require implode(DIRECTORY_SEPARATOR, [BASE_DIR, 'vendor', 'autoload.php']);
+require implode(DIRECTORY_SEPARATOR, [
+    BASE_DIR,
+    'vendor',
+    'autoload.php'
+]);
 
-$mux = new Pux\Mux;
+$mux = new Pux\Mux();
 
-$mux->any('/guestbook/thread/',['Food\Controller\Guestbook\ThreadController','index']);
-$mux->get('/guestbook/thread/view/:token',['Food\Controller\Guestbook\ThreadController','view']);
-$mux->get('/guestbook/thread/create/:title',['Food\Controller\Guestbook\ThreadController','create']);
-$mux->get('/guestbook/thread/delete/:token',['Food\Controller\Guestbook\ThreadController','delete']);
-$mux->get('/guestbook/thread/edit/:token/:title',['Food\Controller\Guestbook\ThreadController','edit']);
+$mux->any('/guestbook/thread/', [
+    'Food\Controller\Guestbook\ThreadController',
+    'index'
+]);
+$mux->get('/guestbook/thread/view/:token', [
+    'Food\Controller\Guestbook\ThreadController',
+    'view'
+]);
+$mux->get('/guestbook/thread/create/:title', [
+    'Food\Controller\Guestbook\ThreadController',
+    'create'
+]);
+$mux->get('/guestbook/thread/delete/:token', [
+    'Food\Controller\Guestbook\ThreadController',
+    'delete'
+]);
+$mux->get('/guestbook/thread/edit/:token/:title', [
+    'Food\Controller\Guestbook\ThreadController',
+    'edit'
+]);
 
-$mux->get('/guestbook/post/view/:token',['Food\Controller\Guestbook\PostController','view']);
-$mux->get('/guestbook/post/create/:tid/:content',['Food\Controller\Guestbook\PostController','create']);
-$mux->get('/guestbook/post/delete/:token',['Food\Controller\Guestbook\PostController','delete']);
-$mux->get('/guestbook/post/edit/:token/:content',['Food\Controller\Guestbook\PostController','edit']);
+$mux->get('/guestbook/post/view/:token', [
+    'Food\Controller\Guestbook\PostController',
+    'view'
+]);
+$mux->get('/guestbook/post/create/:tid/:content', [
+    'Food\Controller\Guestbook\PostController',
+    'create'
+]);
+$mux->get('/guestbook/post/delete/:token', [
+    'Food\Controller\Guestbook\PostController',
+    'delete'
+]);
+$mux->get('/guestbook/post/edit/:token/:content', [
+    'Food\Controller\Guestbook\PostController',
+    'edit'
+]);
 
+$mux->get('/users', [
+    'Food\Controller\User\UserController',
+    'getAllUser'
+]);
 
+$mux->post('/user', [
+    'Food\Controller\User\UserController',
+    'addUser'
+]);
 
-$mux->get('/users', ['Food\Controller\User\UserController', 'getAllUser']);
+$mux->get('/user/:name', [
+    'Food\Controller\User\UserController',
+    'getUserByName'
+], [
+    'require' => [
+        'name' => '([a-z0-9]{8,16})'
+    ]
+]);
 
-$mux->post('/user', ['Food\Controller\User\UserController', 'addUser']);
+$mux->put('/user/:name', [
+    'Food\Controller\User\UserController',
+    'editUser'
+], [
+    'require' => [
+        'name' => '([a-z0-9]{8,16})'
+    ]
+]);
 
-$mux->get(
-    '/user/:name',
-    ['Food\Controller\User\UserController', 'getUserByName'],
-    ['require' => [ 'name' => '([a-z0-9]{8,16})']]
-);
-
-$mux->put(
-    '/user/:name',
-    ['Food\Controller\User\UserController', 'editUser'],
-    ['require' => [ 'name' => '([a-z0-9]{8,16})']]
-);
-
-$mux->delete(
-    '/user/:name',
-    ['Food\Controller\User\UserController', 'deleteUser'],
-    ['require' => [ 'name' => '([a-z0-9]{8,16})']]
-);
-
+$mux->delete('/user/:name', [
+    'Food\Controller\User\UserController',
+    'deleteUser'
+], [
+    'require' => [
+        'name' => '([a-z0-9]{8,16})'
+    ]
+]);
 
 // owen
 $mux->get('/album/index', [
     'Food\Controller\Album\AlbumController',
     'index'
 ]);
+$mux->get('/album/allphoto/:id', [
+    'Food\Controller\Album\AlbumController',
+    'allphoto'
+]);
 $mux->get('/album/create', [
     'Food\Controller\Album\AlbumController',
     'create'
 ]);
-$mux->get('/album/edit', [
+$mux->get('/album/edit/:id', [
     'Food\Controller\Album\AlbumController',
     'edit'
 ]);
-$mux->get('/album/del', [
+$mux->get('/album/del/:id', [
     'Food\Controller\Album\AlbumController',
     'del'
 ]);
-$mux->get('/photo/create', [
+$mux->any('/photo/doupload/:id', [
     'Food\Controller\Album\PhotoController',
-    'create'
+    'doupload'
 ]);
-$mux->get('/photo/edit', [
+$mux->get('/photo/edit/:id', [
     'Food\Controller\Album\PhotoController',
     'edit'
 ]);
-$mux->get('/photo/del', [
+$mux->get('/photo/del/:id', [
     'Food\Controller\Album\PhotoController',
     'del'
 ]);
-$mux->get('/photo/chosen', [
+$mux->get('/photo/show/:id', [
     'Food\Controller\Album\PhotoController',
-    'chosen'
+    'show'
 ]);
-
-$mux->sort();
 
 return $mux;
--- src/Food/Controller/Album/AlbumController.php
+++ src/Food/Controller/Album/AlbumController.php
@@ -4,21 +4,22 @@ namespace Food\Controller\Album;
 use Fruit\Seed;
 use Fruit\Model;
 use Food\Model\Album\Album;
+use Food\Model\Album\Photo;
 
 class AlbumController extends Seed
 {
 
     public function index()
     {
-        $aaa = Album::listAll();
-        $img = array();
-        foreach ($aaa as $a)
-        {
+        $albums = Album::listAll();
+        foreach ($albums as $a) {
             $album = Album::load($a);
-            $allalbum[$a] = [$album->getTitle(),$album->getDescription(),$album->getToken()];
-            
+            $allalbum[$a] = [
+                $album->getTitle(),
+                $album->getDescription(),
+                $album->getToken()
+            ];
         }
-
         return json_encode($allalbum);
     }
 
@@ -35,4 +36,47 @@ class AlbumController extends Seed
             ];
         }
         return json_encode($allphoto);
+    }
+
+    public function create()
+    {
+        $t = $_POST["title"];
+        $d = $_POST["desc"];
+        $album = Album::create($t, $d);
+        $aaa["0"] = [
+            "title" => $album->getTitle(),
+            "desc" => $album->getDescription(),
+            "create" => $album->getCreate()
+        ];
+        return json_encode($aaa);
+    }
+
+    public function edit($id)
+    {
+        $t = $_POST["title"];
+        $d = $_POST["desc"];
+        $album = Album::load($id);
+        $album->setTitle($t);
+        $album->setDescription($d);
+        $album->save();
+        $end = [
+            "ending" => 'edit okay'
+        ];
+        return json_encode($end);
+    }
+
+    public function del($id)
+    {
+        $album = Album::load($id);
+        $aaa = $album->listPhoto();
+        foreach ($aaa as $a) {
+            $photo = Photo::load($a);
+            $photo->delete();
+        }
+        $album->delete();
+        $end = [
+            "ending" => 'delete okay'
+        ];
+        return json_encode($end);
+    }
 }
