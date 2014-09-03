1655ab2052d1f5f5b14de065c88937046e355357
owen_lin <owen80127@gmail.com>
2014-09-03 08:59:00 +0800

    test

--- src/Food/Controller/Album/PhotoController.php
+++ src/Food/Controller/Album/PhotoController.php
@@ -9,47 +9,78 @@ use Food\Model\Album\Album;
 class PhotoController extends Seed
 {
 
-    public function index()
-    {}
-
-    public function create()
+    public function upload()
     {
-        $id = '2';
-        $album = Album::load($id);
-        $path = self::getConfig()->base() . '/image/ccc.jpg';
-        $title = 'aaa';
-        $desc = 'bbb';
-        $photo = Photo::create($album, $path, $title);
+        $loader = new \Twig_Loader_Filesystem(BASE_DIR . '/templates');
+        $twig = new \Twig_Environment($loader);
+        $result[0] = '';
+        echo $twig->render('upload.html', array(
+            'res' => $result
+        ));
     }
 
-    public function edit()
+    public function doupload($id)
     {
-        $id = 'ul_1Qiqld';
-        $t = '0101';
-        $d = 'tyjudrj躲';
+        if ($_FILES["file"]["error"] > 0) {
+            $msg = '1';
+            echo '1';
+        } else {
+            if (file_exists($_FILES["file"]["name"])) {
+                
+                $msg = '2';
+                echo '2';
+            } else {
+                $album = Album::load($id);
+                $t = $_POST["title"];
+                $d = $_POST["desc"];
+                $photo = Photo::create($album, $_FILES["file"]["tmp_name"], $t, $d);
+                $end = [
+                    "end" => "ok"
+                ];
+                return json_encode($end);
+                exit();
+            }
+        }
+        switch ($msg) {
+            case "1":
+            case "2":
+                echo "files type error!";
+                break;
+            default:
+                $msg = "3";
+                echo "call Owen!";
+                break;
+        }
+    }
+
+    public function edit($id)
+    {
+        $t = $_POST["title"];
+        $d = $_POST["desc"];
         $photo = Photo::load($id);
         $photo->setTitle($t);
         $photo->setDescription($d);
         $photo->save();
+        $end = [
+        "end" => "ok"
+            ];
+        return json_encode($end);
     }
 
-    public function del()
+    public function del($id)
     {
-        $id = 'ul_g1PCTN';
         $photo = Photo::load($id);
         $photo->delete();
+        $end = [
+        "end" => "ok"
+            ];
+        return json_encode($end);
     }
-    public function chosen()
-    {
-        $id = 'ul_1Qiqld';
-        $photo = Photo::load($id);
-        $aaa["0"] = array(
-            "title" => $photo->getTitle(),
-            "desc" => $photo->getDescription()
-        );
-        echo json_encode($aaa,JSON_UNESCAPED_UNICODE);
-        $photo->readFile();
 
-        
+    public function show($id)
+    {
+        $photo = Photo::load($id);
+        header('Content-Type: image/jpeg');
+        $photo->readFile();
     }
 }
