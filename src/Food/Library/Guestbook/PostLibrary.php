5aa89456dabd1934e95976d50981d9f11b96db6c
James-Yuan <show760@hotmail.com>
2014-09-16 15:53:44 +0800

    fix guestbook library

--- src/Food/Library/Guestbook/PostLibrary.php
+++ src/Food/Library/Guestbook/PostLibrary.php
@@ -51,117 +51,39 @@ class PostLibrary extends Seed
      * 新增內容
      * @param  int $tid 代碼
      * @param  string $content 內容
-     * @return array(
-     *     'success' => bool,
-     *     'string' => msg,
-     *     'token' => token
-     * )
+     * @return Post object or null
      */
-    public function create($tid = null, $content = null)
+    public function create($tid, $content)
     {
         $thread = Thread::load($tid);
         $content_rule  = '/^.{1,65535}$/';
         $content  = htmlentities(trim($content), ENT_QUOTES, 'UTF-8');
-        if (is_null($thread)) {
-            $msg = array(
-                'success' => false,
-                'string' => 'Error. Could not find thread.'
-            );
-        } elseif (preg_match($content_rule, $content)) {
-            $post = Post::create($thread, $content);
-            if ($post instanceof Post) {
-                $msg = array(
-                    'success' => true,
-                    'string' => 'Success added post ' .$post->getContent(). '.',
-                    'token' => $post->getToken()
-                );
-            } else {
-                $msg = array(
-                    'success' => false,
-                    'string' => 'Error. add post ' .$content. ' failed. Please try again.'
-                );
-            }
-        } else {
-            $msg = array(
-                'success' => false,
-                'string' => 'Error. Input string is not valid.'
-            );
-        }
-        return $msg;
-    }
-    /** 
-     * 刪除內容
-     * @param  int $token 代碼
-     * @return array(
-     *     'success' => bool,
-     *     'string' = >msg
-     * )
-     */
-    public function delete($token = null)
-    {
-        if (is_null($token)) {
-            $msg = array(
-                'success' => false,
-                'string' => 'Error. Token is null.'
-            );
-        } else {
-            $post = Post::load($token);
-            if ($post instanceof Post) {
-                $post->delete();
-                $msg = array(
-                    'success' => true,
-                    'string' => 'Success deleted post.'
-                );
-            } else {
-                $msg = array(
-                    'success' => false,
-                    'string' => 'Error. Could not find token #' .$token. '.'
-                );
+        if ($thread instanceof Thread) {
+            if (preg_match($content_rule, $content)) {
+                $post = Post::create($thread, $content);
+                if ($post instanceof Post) {
+                    return $post;
+                }
             }
         }
-        return $msg;
+        $post = null;
+        return $post;
     }
+
     /** 
      * 修改內容
-     * @param  int $token 代碼
+     * @param  Post $p Post object
      * @param  string $content 內容
-     * @return array(
-     *     'success' => bool,
-     *     'string' => msg,
-     *     'token' => token
-     * )
      */
-    public function edit($token = null, $content = null)
+    public function edit($p, $content)
     {
         $content_rule  = '/^.{1,65535}$/';
         $content  = htmlentities(trim($content), ENT_QUOTES, 'UTF-8');
-        if (is_null($token)) {
-            $msg = array(
-                'success' => false,
-                'string' => 'Error. Token is null.'
-            );
-        } elseif (preg_match($content_rule, $content)) {
-            $post = Post::load($token);
-            if ($post instanceof Post) {
-                $post->setContent($content);
-                $post->save();
-                $msg = array(
-                    'success' => true,
-                    'string' => 'Success updated Post.',
-                    'token' => $token
-                );
-            } else {
-                $msg = array(
-                    'success' => false,
-                    'string' => 'Error. Could not find token ' .$token. '.'
-                );
+        if (preg_match($content_rule, $content)) {
+            if ($p instanceof Post) {
+                $p->setContent($content);
+                $p->save();
             }
-        } else {
-            $msg = array(
-                'success' => false,
-                'string' => 'Error. Input string is not valid.'
-            );
         }
-        return $msg;
     }
 }
